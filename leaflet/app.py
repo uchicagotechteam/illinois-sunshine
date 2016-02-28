e5c094ccfe5eabb650053708a3af879609e10dd9
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2016-02-28 01:51:16 -0600

    Converted to geojson and plotted. Need to get choropleth feature working.

--- leaflet/app.py
+++ leaflet/app.py
@@ -6,9 +6,8 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
 	data = open('./tmp/com_data.json', 'r').read()
-	
-	return flask.render_template("map.html", data=data)
-	#return 'hello,world'
+	geodata = open('./tmp/geojson_com_data.json', 'r').read()
+	return flask.render_template("map.html", geodata=geodata)
 	
 if __name__ == '__main__':
 	app.debug = True
--- leaflet/data.py
+++ leaflet/data.py
@@ -1,4 +1,3 @@
-
 import matplotlib
 import pandas as pd
 import numpy as np
@@ -65,19 +64,19 @@ def main():
 				# c[str(yr) + '_receipt_counter'] = 0
 		
 		for yr in range(1994, 2017):
-			c[str(yr) + '_receipt_volume'] = 0
-			c[str(yr) + '_receipt_counter'] = 0
+			c['receiptVolume' + str(yr)] = 0
+			c['receiptCounter' + str(yr)] = 0
 			
 		resp_rec = requests.get(url + 'receipts/?committee_id=' + str(c['id']))
 		
 		if resp_rec.json()['meta']['total_rows'] != 0:
 			receipts = resp_rec.json()['objects'][0]['receipts']
-			print(len(receipts))
+			#print(len(receipts))
 			for r in receipts:
 				received_yr = datetime.datetime.strptime(r['received_date'], '%Y-%m-%dT%H:%M:%S').year
 				amt = r['amount']
-				c[str(received_yr) + '_receipt_volume'] += amt
-				c[str(received_yr) + '_receipt_counter'] += 1
+				c['receiptVolume' + str(received_yr)] += amt
+				c['receiptCounter' + str(received_yr)] += 1
 			
 			
 		#Write data to csv file
@@ -86,124 +85,37 @@ def main():
 		#	csvwriter.writerow(header)
 		#	count += 1
 		#csvwriter.writerow(c.values())
-	with open('./tmp/com_data.txt', 'w') as outfile:	
-		json.dumps(coms, outfile)
+	geojson = { "type": "FeatureCollection", "features": [{"type": "Feature", "geometry": {"type": "Point", 'coordinates': [c['lon'], c['lat']]},"properties": c } for c in coms]}
+	
+	#print geojson
+	with open('./tmp/com_data.json', 'w') as outfile:	
+		outfile.write(json.dumps(coms))
+	
+	with open('./tmp/geojson_com_data.json', 'w') as outfile:
+		outfile.write(json.dumps(geojson))
 
-def agg_receipts(cid, yr):
-	resp_rec = requests.get(url + 'receipts/?committee_id=' + str(cid) + '&received_date__lt=' + str(yr+1)
-			+ '-01-01T00:00:00&received_date__ge=' + str(yr) + '-01-01T00:00:00') #Get receipts for the cid committee within the year yr
+# def agg_receipts(cid, yr):
+	# resp_rec = requests.get(url + 'receipts/?committee_id=' + str(cid) + '&received_date__lt=' + str(yr+1)
+			# + '-01-01T00:00:00&received_date__ge=' + str(yr) + '-01-01T00:00:00') #Get receipts for the cid committee within the year yr
 	
-	#Store volume and counter within dict
-	dict = {}
-	if(resp_rec.json()['meta']['total_rows'] == 0):
-		dict['receipt_volume'] = 0
-		dict['receipt_counter'] = 0
-		return dict
+	# #Store volume and counter within dict
+	# dict = {}
+	# if(resp_rec.json()['meta']['total_rows'] == 0):
+		# dict['receipt_volume'] = 0
+		# dict['receipt_counter'] = 0
+		# return dict
 	
-	com_rec = resp_rec.json()['objects'][0]['receipts'] #0 gives us the committee {only object in objects list}
+	# com_rec = resp_rec.json()['objects'][0]['receipts'] #0 gives us the committee {only object in objects list}
 	
-	#Loop through receipts to calculate volume and counter
-	receipt_volume = 0
-	receipt_counter = 0
-	for i in range(0, len(com_rec)):
-		receipt_volume += (com_rec[i]['amount'])
-		receipt_counter += 1
-	dict['receipt_volume'] = receipt_volume
-	dict['receipt_counter'] = receipt_counter
-	return dict
-	
-	
-	
-	#comYear = [{} for x in range(1994, 2016)] #This is a list of dictionaries with the index of the list corresponding to the year (0 = 1994)
-											  #So for the year 1994 (index = 0), there will be a dictionary which has keys of a committee id and 
-											  #a value of dictionary holding receipt_volume, receipt_counter, and other stuff.
-											  #So 1 entry in the list would look like [{committee_id1 : {receipt_volume : a, receipt_counter: b}, committee_id2 : {receipt_volume: x, receipt_counter: y}]
-											  #I thought this would be easier to index by years if we were to implement the slider feature by year in the heat map. This may not be the best way to organize
-											  #the data though.
-	
-	##The following block of commented code is an attempt to make the whole process of pulling data faster.
-	##I was trying to implement running http requests simultaneously using a module called grequests (which I learned about on stackoverflow)
-	##However the module seems to be outdated as it only is able to call google.com and bing.com. I may be trying to implement something
-	##with FuturesSessions, but if you have any ideas on how to make multiple http requests simultaneously, please let me know.
-	
-	#List of committee ids
-	# cids = [0 for x in range(0, len(coms))]
-	# for j in range(0, len(coms)):
-		# cids[j] = coms[j]['id']
-		
-	#Store urls to call in multiple instances
-	# urls = []
-	# for k in range(1994, 2016):
-		# for j in range(len(cids)):
-			# #Get urls for all cids for kth year
-			# urls.append(url + 'receipts/?committee_id=' + str(cids[j]) + '&received_date__lt=' + str(k+1)
-			# + '-01-01T00:00:00&received_date__ge=' + str(k) + '-01-01T00:00:00')
-		
-		# #Call request for all cids for kth year
-		# rs = (grequests.get(u) for u in urls)
-		# responses = grequests.map(rs)
-		
-		# #Calculate total receipts etc for each cid for kth year
-		# for a in responses:
-			# dict = {}
-			# receipt_volume = 0
-			# receipt_counter = 0
-			# com_rec = a.json()['objects'][0]['receipts']
-			# for i in range(0, len(com_rec)):
-				# receipt_volume += com_rec[i]['amount']
-				# receipt_counter += 1
-			# dict['receipt_volume'] = receipt_volume
-			# dict['receipt_counter'] = receipt_counter
-			# comYear[k-1994][str(a.json()['objects'][0]['id'])] = dict
-			# a.close()
-		
-		# #Clear urls for k+1 year
-		# urls[:] = []
-	
-	
-	#Loop through each committee
-	#for j in range(0, len(coms)):
-	#	cid = coms[j]['id']
-		#Need to seperate by year
-	#	for k in range(1994, 2016):
-			#resp_rec = requests.get(url + 'receipts/?committee_id=' + str(cid) + '&received_date__lt=' + str(k+1)
-			#+ '-01-01T00:00:00&received_date__ge=' + str(k) + '-01-01T00:00:00') #Get receipts for the jth committee within the kth year
-			
-	#		resp_rec = sess.get(url + 'receipts/?committee_id=' + str(cid) + '&received_date__lt=' + str(k+1)
-	#		+ '-01-01T00:00:00&received_date__ge=' + str(k) + '-01-01T00:00:00')
-			
-			#If no receipts for this committee, then set receipt_counter and receipt_volunteer = 0
-	#		if(resp_rec.json()['meta']['total_rows'] == 0):
-	#			dict = {'receipt_volume': 0}
-	#			dict['receipt_counter'] = 0
-	#			continue
-				
-	#		com_rec = resp_rec.json()['objects'][0]['receipts'] #0 gives us the committee {only object in objects list}
-			
-	#		dict = {}
-	#		receipt_volume = 0
-	#		receipt_counter = 0
-	#		for i in range(0, len(com_rec)):
-	#			receipt_volume += (com_rec[i]['amount'])
-	#			receipt_counter += 1
-	#		dict['receipt_volume'] = receipt_volume
-	#		dict['receipt_counter'] = receipt_counter
-	#		comYear[k-1994][str(cid)] = dict
-		
-	#	print(comYear)
-	
-	#for i in range(1, len(respC.json()['objects'])):
-	#	cid = respC.json()['objects'][i]['id']
-	#	print(respC.json()['objects'][i]['id'])
-	#	respR = requests.get(url + 'receipts/?committee_id=' + str(cid))
-	#	print(respR)
-	#	comId.append(cid)
-	#	break
-	#print(len(comId))
-	#obj = json.loads(resp.json())
-	#print()
-	
-	
+	# #Loop through receipts to calculate volume and counter
+	# receipt_volume = 0
+	# receipt_counter = 0
+	# for i in range(0, len(com_rec)):
+		# receipt_volume += (com_rec[i]['amount'])
+		# receipt_counter += 1
+	# dict['receipt_volume'] = receipt_volume
+	# dict['receipt_counter'] = receipt_counter
+	# return dict
 	
 if __name__ == '__main__':
 	main()
