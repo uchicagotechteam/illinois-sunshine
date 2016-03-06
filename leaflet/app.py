54e95f94ef67274ae4b7b59bef475ec04ed3138a
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2016-03-06 00:02:25 -0600

    Implemented cycling of receipt counter years with corresponding year shown in top left. Next steps are to implement a user interface so that the user can choose between receipt_counter, receipt_volume, exp_volume, exp_counter, etc. Also, need to implement a pause functionality or a timeline scrubbing functionality rather than simply looping through layers. Makes it easier for user to understand the data.

--- leaflet/app.py
+++ leaflet/app.py
@@ -7,8 +7,9 @@ app = flask.Flask(__name__)
 def index():
 	data = open('./tmp/com_data.json', 'r').read()
 	geodata = open('./tmp/geojson_com_data.json', 'r').read()
-	timelineURL = flask.url_for('static', filename='Leaflet.TimeDimension/src/leaflet.timedimension.js')
-	return flask.render_template("map.html", geodata=geodata, turl = timelineURL)
+	timelineURL = flask.url_for('static', filename='Leaflet.TimeDimension/src')
+	dateUrl = flask.url_for('static', filename = 'iso8601-js-period')
+	return flask.render_template("map.html", geodata=geodata, turl = timelineURL, durl = dateUrl)
 	
 if __name__ == '__main__':
 	app.debug = True
